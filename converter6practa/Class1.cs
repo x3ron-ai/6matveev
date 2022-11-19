777f19da94082102d48a6d3f28b3afa76f11c97d
x3ron-ai <69518463+x3ron-ai@users.noreply.github.com>
2022-11-19 18:46:45 +0300

    ya prosto negr??

---
+++ converter6practa/Class1.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace converter6practa
+{
+    public class Figura
+    {
+        public string name;
+        public int width;
+        public int height;
+        public Figura()
+        {
+
+        }
+        public Figura(string Name, int Height, int Width)
+        {
+            name=Name;
+            height=Height;
+            width=Width;
+        }
+    }
+    public class Preobrazovanie
+    {
+        private static List<Figura> ConvertToObject(string file)
+        {
+            List<Figura> figury = new List<Figura>();
+            if (file.Contains(".xml"))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Figura>));
+                using (FileStream fs = new FileStream(file, FileMode.Open))
+                {
+                    figury = (List<Figura>)xml.Deserialize(fs);
+                }
+            }
+            if (file.Contains(".json"))
+            {
+                figury = JsonConvert.DeserializeObject<List<Figura>>(File.ReadAllText(file));
+            }
+
+            if (file.Contains(".txt"))
+            {
+                string[] linii = File.ReadAllLines(file);
+                for (int i = 0; i < linii.GetLength(0); i = i + 3)
+                {
+                    Figura figura = new Figura();
+                    if (i != linii.GetLength(0))
+                    {
+                        figura.name = linii[i];
+                    }
+                    else break;
+                    if (i + 1 != linii.GetLength(0))
+                    {
+                        figura.width = Convert.ToInt32(linii[i + 1]);
+
+                    }
+                    else break;
+                    if (i + 2 != linii.GetLength(0))
+                    {
+                        figura.height = Convert.ToInt32(linii[i + 2]);
+                    }
+                    else break;
+                    figury.Add(figura);
+                }
+            }
+            return figury;
+        }
+        public static string ConvertToText(string file)
+        {
+            string text = "";
+            List<Figura> figury = ConvertToObject(file);
+            for (int i = 0; i < figury.Count(); i++)
+            {
+                text = text + figury[i].name + "\n";
+                text = text + figury[i].height + "\n";
+                text = text + figury[i].width + "\n";
+            }
+            return text;
+        }
+        public static void SaveFile(string oldFile, string newFile)
+        {
+            List<Figura> figury = ConvertToObject(oldFile);
+            if (newFile.Contains(".xml"))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Figura>));
+                using (FileStream fs = new FileStream(newFile, FileMode.OpenOrCreate))
+                {
+                    xml.Serialize(fs, figury);
+                }
+            }
+            if (newFile.Contains(".json"))
+            {
+                File.WriteAllText(newFile, JsonConvert.SerializeObject(figury));
+            }
+            if (newFile.Contains(".txt"))
+            {
+                File.WriteAllText(newFile, ConvertToText(newFile));
+            }
+        }
+    }
+}
---
+++ converter6practa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+namespace converter6practa
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Путь до файла");
+            string putDoFaila = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Нажмите F1 чтобы сохранить файл");
+            Console.WriteLine(Preobrazovanie.ConvertToText(putDoFaila));
+            while (true) {
+                if (Console.ReadKey().Key == ConsoleKey.F1)
+                {
+                    break;
+                };
+            }
+            Console.Clear();
+            Console.WriteLine("Путь до файла чтобы сохранить");
+            string putDlyaSohranenia = Console.ReadLine();
+            Preobrazovanie.SaveFile(putDoFaila, putDlyaSohranenia);
+            Console.Clear();
+            Console.WriteLine("Сохранено!");
+
+        }
+    }
+}
